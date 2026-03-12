cd231404e362e7ffc5eb9e4e194ec53e1f987775
Wilson Dias Debus <wilsonddebus@gmail.com>
2026-03-12 18:59:02 -0300

    Update exer7.java

--- Aula 1/exerciciosAula1/exer7.java
+++ Aula 1/exerciciosAula1/exer7.java
@@ -1,1 +1,27 @@
+import java.util.Scanner; 
 
+public class exer7 {
+    public static void main(String[]args){
+
+        Scanner teclado = new Scanner(System.in); 
+
+        float num1, num2;
+
+        System.out.println("Digite dois valores para verificar se são multiplos um do outro: ");
+        num1 = teclado.nextFloat();
+        num2 = teclado.nextFloat();
+
+        if(num1 % num2 == 0){
+            System.out.println("O numero "+num1+" é mulpliplo do numero "+num2);
+        } else {
+            System.out.println("O numero "+num1+" NÃO é mulpliplo do numero "+num2);
+        }
+        if(num2 % num1 == 0){
+            System.out.println("O numero "+num2+" é mulpliplo do numero "+num1);
+        } else {
+            System.out.println("O numero "+num2+" é mulpliplo do numero "+num1);
+        }
+
+        teclado.close(); 
+    }
+}
