3ef6a019e33bd66d3a603521677cff839f962366
Wilson Dias Debus <wilsonddebus@gmail.com>
2026-03-12 18:46:28 -0300

    Create Computador.java

---
+++ Aula 3/Exercicio 2/Computador.java
@@ -0,0 +1,9 @@
+public class Computador {
+
+    public String marca = "Lenovo"; 
+    public String modelo = "LOQ"; 
+    public String tipo = "Notbook"; 
+    public float preco = 4500;  
+
+        
+}
