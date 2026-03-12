2bf01972a288843c31faade14a232be7e0504454
Wilson Dias Debus <wilsonddebus@gmail.com>
2026-03-12 18:57:38 -0300

    Update exer2.java

--- Aula 1/exerciciosAula1/exer2.java
+++ Aula 1/exerciciosAula1/exer2.java
@@ -1,1 +1,32 @@
+import java.util.Scanner;
 
+public class exer2 {
+    public static void main(String[]args){
+    float precoCombustivel = 4.90f; 
+    //leitura
+    float marcacaoOdometroInicial, marcacaoOdometroFinal, numeroLitrosGastos, valorRecebido; 
+    //calcular 
+    float quilometragemTotal, media, lucroLiquido;
+
+    Scanner teclado = new Scanner(System.in);
+
+    System.out.println("Digite a marcação do odômetro inicial: "); 
+    marcacaoOdometroInicial = teclado.nextFloat(); 
+    System.out.println("Digite a marcação do odôpometro final: "); 
+    marcacaoOdometroFinal = teclado.nextFloat(); 
+    System.out.println("Digite o número de litros gastos: "); 
+    numeroLitrosGastos = teclado.nextFloat(); 
+    System.out.println("Digite o valor recebido pelo taxista: ");
+    valorRecebido = teclado.nextFloat(); 
+
+    quilometragemTotal = marcacaoOdometroFinal - marcacaoOdometroInicial; 
+    media = quilometragemTotal / numeroLitrosGastos; 
+    lucroLiquido = valorRecebido - (numeroLitrosGastos * precoCombustivel); 
+
+    System.out.println("A media do consumo em Km/l é: "+media);
+    System.out.println("O lucro líquido do dia foi: "+lucroLiquido);
+
+    teclado.close();
+    }
+
+}
