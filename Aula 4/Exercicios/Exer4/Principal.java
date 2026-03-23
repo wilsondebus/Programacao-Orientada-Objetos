af6158f1260bbb247f4e8dd1004ed0cf3cecc4bf
Wilson Dias Debus <wilsonddebus@gmail.com>
2026-03-23 20:56:20 -0300

    Create Principal.java

---
+++ Aula 4/Exercicios/Exer4/Principal.java
@@ -0,0 +1,10 @@
+package Exercicio4;
+
+public class Principal{
+    public static void main(String[] args) {
+
+        ContaCorrente c1 = new Exercicio4.ContaCorrente();
+
+        c1.definirSaldoInicial();
+    }
+}
