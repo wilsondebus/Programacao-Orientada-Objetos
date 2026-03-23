8a0b521a545a121ca8cb00e6170c9ba5ac95e406
Wilson Dias Debus <wilsonddebus@gmail.com>
2026-03-23 20:57:55 -0300

    Create Principal.java

---
+++ Aula 4/Exercicios/Exer6/Principal.java
@@ -0,0 +1,12 @@
+package Exercicio6;
+
+public class Principal {
+    public static void main(String[] args) {
+
+        Aluno a1 = new Aluno("Wilson", "20241234");
+
+        Aluno a2 = new Aluno("01/01/2000");
+
+        Aluno a3 = new Aluno("Maria", "02/02/1999", 2023);
+    }
+}
