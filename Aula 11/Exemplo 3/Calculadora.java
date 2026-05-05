e463431d5bb2dc0a9b01c40cf4fcb10c1bcbd12d
wilsondebus <wilsonddebus@gmail.com>
2026-05-05 10:46:07 -0300

    Adicionando exercicios

---
+++ Aula 11/Exemplo 3/Calculadora.java
@@ -0,0 +1,22 @@
+public class Calculadora{
+
+    public int somar(int x, int y){
+        return x + y;
+    }
+
+    public int somar(int x, int y, int z){
+        return x + y + z;
+    }
+
+    public double somar(double x, double y){
+        return x + y; 
+    }
+
+    public int multiplica(int x, int y){
+        return x * y; 
+    }
+
+    public double multiplica(double x, double y){
+        return x * y;
+    }
+}
---
+++ Aula 11/Exemplo 4/Pessoa.java
@@ -0,0 +1,10 @@
+public class Pessoa{
+
+    public void dizerOla(){
+        System.out.println("Olá!");
+    }
+
+    public void dizerOla(String nome){
+        System.out.println("Olá, "+nome+"!");
+    }
+}
